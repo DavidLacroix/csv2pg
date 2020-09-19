f34ecf37c379c0329ded62cbf7397a366b5bfb21
David Lacroix <dlacroix@meilleursagents.com>
2020-09-19 19:06:47 +0200

    feat: basic Makefile for devs

--- setup.py
+++ setup.py
@@ -6,9 +6,17 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='csv2pg',
-    version='1.0.0b2',
+    version=get_version('csv2pg/__init__.py'),
     url='https://github.com/DavidLacroix/csv2pg',
     description='A simple and fast cli application to load a csv into postgres',
     long_description=README,
@@ -23,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     keywords='postgres csv',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     install_requires=[
         'Click',
